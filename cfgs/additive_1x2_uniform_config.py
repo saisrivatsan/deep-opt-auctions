bdfa49dcac49f8adf72e1c60a448a35045076c13
Sai Ravindranath <sravindranath@seasdgx104.rc.fas.harvard.edu>
2019-01-23 15:10:31 -0500

    updated rev rgt plots

--- cfgs/additive_1x2_uniform_config.py
+++ cfgs/additive_1x2_uniform_config.py
@@ -12,7 +12,7 @@ __C = edict()
 cfg = __C
 
 # Output-dir to write log-files and save model
-__C.dir_name = os.path.join("experiments", "additive_1x2_uniform_2")
+__C.dir_name = os.path.join("experiments", "additive_1x2_uniform")
 
 # Auction params
 __C.num_agents = 1
@@ -77,9 +77,9 @@ __C.train.update_rate = 1.0
 __C.train.w_rgt_init_val = 5.0
 # Lagrange update frequency
 __C.train.update_frequency = 100
-# Value by which update rate is incremented-
+# Value by which update rate is incremented
 __C.train.up_op_add = 50.0
-# Frequency at which update rate is incremented-
+# Frequency at which update rate is incremented
 __C.train.up_op_frequency = 10000
 
 

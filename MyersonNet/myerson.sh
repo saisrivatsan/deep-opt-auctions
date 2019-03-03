62afee013827174ceb9e9172e6a36a639d2ff00e
Zhe Feng <zhe_feng@g.harvard.edu>
2019-03-03 14:55:42 -0500

    remove duplicated myersonNet

--- MyersonNet/myerson.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python main.py -agent 3 -item 1 -distr exponential -num_linear 10 -num_max 5 &
-python main.py -agent 3 -item 1 -distr uniform -num_linear 10 -num_max 10 &
-python main.py -agent 5 -item 1 -distr asymmetric_uniform -num_linear 10 -num_max 10 &
-python main.py -agent 3 -item 1 -distr irregular -num_linear 10 -num_max 5 &

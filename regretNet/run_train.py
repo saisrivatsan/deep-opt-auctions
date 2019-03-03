02b93093aca482d176c3848b86ac0374bc79e541
Sai Srivatsa R <saisrivatsan12@gmail.com>
2019-03-03 16:46:00 -0500

    merged run_train.py and run_train_additional.py

--- regretNet/run_train.py
+++ regretNet/run_train.py
@@ -72,6 +72,48 @@ elif setting == "CA_sym_uniform_12":
     clip_op_lambda = (lambda x: clip_op_12(x))
     Trainer = ca12_2x2.Trainer
 
+elif setting == "additive_1x2_uniform_416_47":
+    cfg = additive_1x2_uniform_416_47_config.cfg
+    Net = additive_net.Net
+    Generator = uniform_416_47_generator.Generator
+    clip_op_lambda = (lambda x: clip_op_416_47(x))
+    Trainer = trainer.Trainer
+    
+elif setting == "additive_1x2_uniform_triangle":
+    cfg = additive_1x2_uniform_triangle_config.cfg
+    Net = additive_net.Net
+    Generator = uniform_triangle_01_generator.Generator
+    clip_op_lambda = (lambda x: clip_op_triangle_01(x))
+    Trainer = trainer.Trainer
+    
+elif setting == "unit_1x2_uniform":
+    cfg = unit_1x2_uniform_config.cfg
+    Net = unit_net.Net
+    Generator = uniform_01_generator.Generator
+    clip_op_lambda = (lambda x: clip_op_01(x))
+    Trainer = trainer.Trainer
+
+elif setting == "additive_1x10_uniform":
+    cfg = additive_1x10_uniform_config.cfg
+    Net = additive_net.Net
+    Generator = uniform_01_generator.Generator
+    clip_op_lambda = (lambda x: clip_op_01(x))
+    Trainer = trainer.Trainer
+
+elif setting == "additive_1x2_uniform_04_03":
+    cfg = additive_1x2_uniform_04_03_config.cfg
+    Net = additive_net.Net
+    Generator = uniform_04_03_generator.Generator
+    clip_op_lambda = (lambda x: clip_op_04_03(x))
+    Trainer = trainer.Trainer
+
+elif setting == "unit_2x2_uniform":
+    cfg = unit_2x2_uniform_config.cfg
+    Net = unit_net.Net
+    Generator = uniform_01_generator.Generator
+    clip_op_lambda = (lambda x: clip_op_01(x))
+    Trainer = trainer.Trainer
+    
 else:
     print("None selected")
     sys.exit(0)
